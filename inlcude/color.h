a9fdddd64b28fc17d6d4d77fbff69e749a86c076
root <liangxin1200@163.com>
2013-03-21 08:54:27 +0800

    add color.h

---
+++ inlcude/color.h
@@ -0,0 +1,21 @@
+#ifndef _COLOR_H_DEFINE
+#define _COLOR_H_DEFINE
+
+#define RED(M) "\033[22;31m"M"\033[0m"
+#define GREEN(M) "\033[22;32m"M"\033[0m"
+#define BROWN(M) "\033[22;33m"M"\033[0m"
+#define BLUE(M) "\033[22;34m"M"\033[0m"
+#define MAGENTA(M) "\033[22;35m"M"\033[0m"
+#define CYAN(M) "\033[22;36m"M"\033[0m"
+#define GRAY(M) "\033[22;37m"M"\033[0m"
+#define DGRAY(M) "\033[01;30m"M"\033[0m"
+#define LRED(M) "\033[01;31m"M"\033[0m"
+#define LGREEN(M) "\033[01;32m"M"\033[0m"
+#define YELLOW(M) "\033[00;33m"M"\033[0m"
+#define LBLUE(M) "\033[01;34m"M"\033[0m"
+#define LMAGENTA(M) "\033[01;35m"M"\033[0m"
+#define LCYAN(M) "\033[01;36m"M"\033[0m"
+#define WHITE(M) "\033[01;37m"M"\033[0m"
+#define NORM(M) "\033[0m"M"\033[0m"
+
+#endif
---
+++ test/utils/test_color.c
@@ -0,0 +1,22 @@
+#include "../../inlcude/color.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", RED("liangxin"));
+	printf("%s\n", GREEN("liangxin"));
+	printf("%s\n", BROWN("liangxin"));
+	printf("%s\n", BLUE("liangxin"));
+	printf("%s\n", MAGENTA("liangxin"));
+	printf("%s\n", CYAN("liangxin"));
+	printf("%s\n", DGRAY("liangxin"));
+	printf("%s\n", LRED("liangxin"));
+	printf("%s\n", LGREEN("liangxin"));
+	printf("%s\n", YELLOW("liangxin"));
+	printf("%s\n", LBLUE("liangxin"));
+	printf("%s\n", LMAGENTA("liangxin"));
+	printf("%s\n", LCYAN("liangxin"));
+	printf("%s\n", WHITE("liangxin"));
+	printf("%s\n", NORM("liangxin"));
+	return 0;
+}
